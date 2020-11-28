c27e0e3a1faeeddd01063368be9a6748d63ab32b
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-11-28 10:50:37 -0600

    add a pizza to database - createPizza

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -2,7 +2,7 @@ const { Pizza } = require('../models');
 
 const pizzaController = {
  // the functions will go in here as methods
- 
+
   // get all pizzas
   getAllPizza(req, res) {
     Pizza.find({})
@@ -28,6 +28,12 @@ const pizzaController = {
         console.log(err);
         res.status(400).json(err);
       });
+  },
+  // add a pizza to the database
+  createPizza({ body }, res) {
+    Pizza.create(body)
+      .then(dbPizzaData => res.json(dbPizzaData))
+      .catch(err => res.status(400).json(err));
   },
 }
 
